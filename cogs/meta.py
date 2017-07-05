fbe5ee3bab1be62c69dcbf70d497f336de7d4b0c
Lucien Rae <lucienraegentil@gmail.com>
2017-07-05 14:03:46 +1000

    Added ServerInfo and also an UpTime command

--- cogs/meta.py
+++ cogs/meta.py
@@ -3,6 +3,7 @@ import discord
 import time
 from   discord.ext import commands
 from cogs import readabletime as ReadableTime
+from cogs import style as Style
 
 class BotInfo:
 
@@ -12,7 +13,7 @@ class BotInfo:
 
 	@commands.command(pass_context=True)
 	async def uptime(self, ctx):
-		"""Lists the bot's uptime."""
+		"""Displays the bot's uptime."""
 		currentTime = int(time.time())
 		timeString  = ReadableTime.getReadableTimeBetween(self.startTime, currentTime)
 		msg = "I've been up for *{}*.".format(timeString)
@@ -23,3 +24,43 @@ class ServerInfo:
 
 	def __init__(self, bot):
 		self.bot = bot
+
+	@commands.command(pass_context=True)
+	async def serverinfo(self, ctx):
+		"""Displays server information."""
+		server = ctx.message.server
+		embed = discord.Embed(title = server.name, color=Style.Colors.blue)
+
+		# general
+		embed.add_field(name="General", value="Owner: {}\nMembers: {}\nRegion: {}\nChannels: {}\nAFK Timeout: {}\nLarge: {}\nVerification Level: {}\nDefault Role: {}".format(server.owner.name, server.member_count, str(server.region).title(), len(server.channels), server.afk_timeout, server.large, str(server.verification_level).title(), server.default_role), inline=False)
+
+		# roles
+		roles = []
+		for role in server.role_hierarchy:
+			role_entry = role.name
+			if role.hoist:
+				role_entry = role_entry + "*"
+			if role.permissions.administrator:
+				role_entry = role_entry + " (Admin)"
+			roles.append(role_entry)
+
+		roles_display = '\n'.join(roles)
+
+		embed.add_field(name="Roles", value="{}".format(roles_display), inline=False)
+
+		# emoji
+		emojis = []
+		for emoji in server.emojis:
+			emoji_entry = "{}".format(emoji.name)
+
+			emojis.append(emoji_entry)
+
+		emojis_display = '\n'.join(emojis)
+
+		embed.add_field(name="Emojis", value="{}".format(emojis_display), inline=False)
+
+		
+		# # extra
+		embed.set_footer(text="Server ID: {}".format(server.id))
+
+		await self.bot.say(embed=embed)
--- cogs/style.py
+++ cogs/style.py
@@ -2,3 +2,4 @@ class Colors:
 
 	orange = 0xf27b1a
 	green = 0x1caf32
+	blue = 0x7289da
